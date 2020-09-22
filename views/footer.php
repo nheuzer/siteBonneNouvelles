69c5b659f6726eb42567233abd1ed3b2d8c0f11d
unknown <nina.heuzer@student.vinci.be>
2020-09-22 18:45:23 +0200

    add - my name

--- views/footer.php
+++ views/footer.php
@@ -5,6 +5,7 @@
 				  echo $time; ?>ms pour exécuter le script PHP :: 
 			<!-- ! adresse email non cryptée : spam possible -->
 	        <a href="mailto:<?php echo EMAIL ?>"><?php echo EMAIL ?></a>
+			<p>Je m'appelle Nina Heuzer</p>
 			<p id="copyright">Mise en page d'après
 				<a href="http://www.alsacreations.com/tutoriels/">Alsacréations</a>
 			</p>
