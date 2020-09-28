554159abf51b4a31d62318fcb1980cb7aa10b352
nheuzer <71337967+nheuzer@users.noreply.github.com>
2020-09-28 07:56:52 +0200

    Create views (sauf footer)

---
+++ views/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8" >
+		<title>Site des Bonnes Nouvelles on Azure</title>
+		<link rel="stylesheet" type="text/css" href="views/css/base.css" media="all" >
+		<link rel="stylesheet" type="text/css" href="views/css/modele01.css" media="screen" >
+	</head>
+	<body>
+	<div id="global">
+		<div id="entete">
+			<h1>
+				<a href="index.php">
+				<img src="views/images/smiley.jpg" alt="Sourire">
+				</a>
+				Site des Bonnes Nouvelles on Azure
+			</h1>
+			<p class="sous-titre">
+				<strong>Institut Paul Lambin</strong>
+				:: 1ère année du baccalauréat en informatique
+			</p>
+		</div><!-- entete -->
